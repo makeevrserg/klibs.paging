a394957c9856be662e124c098c5b996cebdb7948
Roman Makeev <makeevrserg@gmail.com>
2026-01-31 13:54:39 +0300

    update agp9

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,28 +6,17 @@ plugins {
     alias(libs.plugins.klibs.gradle.detekt.compose) apply false
     alias(libs.plugins.klibs.gradle.dokka.root) apply false
     alias(libs.plugins.klibs.gradle.dokka.module) apply false
-    alias(libs.plugins.klibs.gradle.java.core) apply false
+    alias(libs.plugins.klibs.gradle.java.version) apply false
     alias(libs.plugins.klibs.gradle.publication) apply false
     alias(libs.plugins.klibs.gradle.rootinfo) apply false
     // klibs - android
-    alias(libs.plugins.klibs.gradle.android.core) apply false
+    alias(libs.plugins.klibs.gradle.android.sdk) apply false
     alias(libs.plugins.klibs.gradle.android.compose) apply false
     alias(libs.plugins.klibs.gradle.android.apk.sign) apply false
     alias(libs.plugins.klibs.gradle.android.apk.name) apply false
+    alias(libs.plugins.klibs.gradle.android.namespace) apply false
 }
 
 apply(plugin = "ru.astrainteractive.gradleplugin.dokka.root")
 apply(plugin = "ru.astrainteractive.gradleplugin.detekt")
 apply(plugin = "ru.astrainteractive.gradleplugin.root.info")
-
-subprojects.forEach { subProject ->
-    subProject.apply(plugin = "ru.astrainteractive.gradleplugin.root.info")
-    subProject.apply(plugin = "ru.astrainteractive.gradleplugin.dokka.module")
-    subProject.apply(plugin = "ru.astrainteractive.gradleplugin.publication")
-    subProject.plugins.withId("org.jetbrains.kotlin.jvm") {
-        subProject.apply(plugin = "ru.astrainteractive.gradleplugin.java.core")
-    }
-    subProject.plugins.withId("com.android.library") {
-        subProject.apply(plugin = "ru.astrainteractive.gradleplugin.android.core")
-    }
-}
